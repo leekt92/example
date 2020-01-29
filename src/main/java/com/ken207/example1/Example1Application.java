282cdb0333f88c20eefb2344a99431090ae2c258
leekt92 <60178626+leekt92@users.noreply.github.com>
2020-01-29 20:14:45 +0900

    신규프로젝트
    시작이반이다.

---
+++ src/main/java/com/ken207/example1/Example1Application.java
@@ -0,0 +1,14 @@
+package com.ken207.example1;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example1Application.class, args);
+	}
+
+}
